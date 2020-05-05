24d3a4cd11126b9ecb7b2513530da987610c18d9
Alejandro Suárez <alejandro.srz@hotmail.com>
2020-05-05 04:37:56 -0500

    feat multilanguage function

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
-interface iRule {
-  title: string;
-  dont: boolean;
-  description: string;
-}
-
-interface iBox {
+type iData = {
   name: string;
   description: string;
+};
+interface iRule extends iData {
+  dont: boolean;
 }
 
-function RenderRules() {
-  $.get("data/rules.json", function (rules: iRule[]) {
+alert("test")
+
+const DEFAULT_LANGUAGE: string = "en";
+const ACCEPT_LANGUAGES = ["es", "en"];
+
+function GetUserLanguage() {
+  let local_language =
+    window.navigator.userLanguage || window.navigator.language;
+  if (local_language) {
+    let language = local_language.split("-")[0];
+    if (ACCEPT_LANGUAGES.includes(language)) {
+      return language;
+    }
+  }
+  return DEFAULT_LANGUAGE;
+}
+
+function getParameterByName(name: string, url?: string) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function RenderRules(language: string) {
+  $.get(`data/${language}/rules.json`, function (rules: iRule[]) {
     let template = rules.map((r) => RuleComponent(r)).join("");
     let dom_rules = document.querySelector("#rules");
     if (dom_rules) dom_rules.innerHTML = template;
@@ -18,13 +42,14 @@ function RenderRules() {
 }
 
 function RuleComponent(rule: iRule): string {
-  let { title, description, dont } = rule;
+  let { name, description, dont } = rule;
 
   return `<div class="item">
       <div class="content">
-        <div class="header"><i class="${dont ? "icon-remove" : "icon-ok"} ${
+        <div class="header">
+        <i class="${dont ? "icon-remove" : "icon-ok"} ${
     dont ? "red" : "green"
-  }"></i> ${title}</div>
+  }"></i> ${name}</div>
         <div class="description">
           ${description}
         </div>
@@ -32,15 +57,15 @@ function RuleComponent(rule: iRule): string {
     </div>`;
 }
 
-function RenderBoxes() {
-  $.get("data/boxes.json", function (rules: iBox[]) {
+function RenderBoxes(language: string) {
+  $.get(`data/${language}/boxes.json`, function (rules: iData[]) {
     let template = rules.map((b) => BoxComponent(b)).join("");
     let dom_boxes = document.querySelector("#boxes");
     if (dom_boxes) dom_boxes.innerHTML = template;
   });
 }
 
-function BoxComponent(box: iBox): string {
+function BoxComponent(box: iData): string {
   let { name, description } = box;
   return `<div class="item">
       <div class="content">
@@ -52,15 +77,15 @@ function BoxComponent(box: iBox): string {
     </div>`;
 }
 
-function RenderCommands() {
-  $.get("data/commands.json", function (rules: iBox[]) {
+function RenderCommands(language: string) {
+  $.get(`data/${language}/commands.json`, function (rules: iData[]) {
     let template = rules.map((b) => CommandComponent(b)).join("");
     let dom_boxes = document.querySelector("#commands");
     if (dom_boxes) dom_boxes.innerHTML = template;
   });
 }
 
-function CommandComponent(box: iBox): string {
+function CommandComponent(box: iData): string {
   let { name, description } = box;
   return `<div class="item">
       <div class="content">
@@ -72,8 +97,31 @@ function CommandComponent(box: iBox): string {
     </div>`;
 }
 
+function RenderMessages(language: string) {
+  if (language == DEFAULT_LANGUAGE) return;
+  $.get(`data/${language}/messages.json`, function (messages: {}) {
+    console.log("fetched");
+    let keys: string[] = Object.keys(messages);
+    let values: string[] = Object.values(messages);
+    for (let i = 0, length = values.length; i < length; i++) {
+      let key = keys[i];
+      let value = values[i].replace("\n", "<br />");
+      let elements = document.getElementsByClassName(`lg_${key}`);
+      for (let j = 0; j < elements.length; j++) {
+        let element = elements[j];
+        element.innerHTML = value;
+      }
+    }
+  });
+}
+
 window.onload = function () {
-  RenderRules();
-  RenderCommands();
-  RenderBoxes();
+  let language = getParameterByName("lang");
+  if (!language) {
+    language = GetUserLanguage();
+  }
+  RenderMessages(language);
+  RenderRules(language);
+  RenderCommands(language);
+  RenderBoxes(language);
 };
